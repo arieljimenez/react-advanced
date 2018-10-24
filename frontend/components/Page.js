bd004190280c89c39220f37b1f5c25bde09d5459
Ariel Jimenez <ariel.jimenez2025@gmail.com>
2018-10-24 08:38:20 -0400

    Add basic Page header meta and nav components

---
+++ frontend/components/Page.js
@@ -0,0 +1,16 @@
+const React = require('react');
+import Header from './Header';
+import Meta from './Meta';
+
+export default class Page extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <Meta />
+        <Header />
+        { this.props.children}
+      </div>
+    )
+  }
+}

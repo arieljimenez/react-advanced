9c7f1f435a990840184e84473f88116d7fc5295e
Ariel Jimenez <ariel.jimenez2025@gmail.com>
2018-12-04 08:09:23 -0400

    Add header styles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,16 +1,35 @@
 const React = require('react');
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
 import Header from './Header';
 import Meta from './Meta';
+import theme from './styles/theme';
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxwidth || '1000px'};
+  background: ${props => props.theme.background || 'white'};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default class Page extends React.Component {
 
   render(){
     return(
-      <div>
-        <Meta />
-        <Header />
-        { this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>
+            { this.props.children }
+          </Inner>
+        </StyledPage>
+      </ThemeProvider>
     )
   }
 }
---
+++ frontend/components/styles/theme.js
@@ -0,0 +1,12 @@
+
+const theme = {
+  background: 'white',
+  red: '#FF0000',
+  black: '#393939',
+  lightgrey: '#E1E1E1',
+  offwhite: 'EDEDED',
+  'max-width': '960px',
+  bs: '0 12px 24px 0 rgba(0,0,0,0.09)'
+};
+
+module.exports = theme;
